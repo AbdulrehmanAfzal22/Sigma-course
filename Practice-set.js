53084d725741f2b7c87840d13819b416816479cd
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-05-02 20:05:46 +0500

    Javascript tutorial

---
+++ Practice-set.js
@@ -0,0 +1,24 @@
+
+let a = "Ali"
+let b = 6
+console.log(a + b)
+
+console.log(typeof (a+b))
+
+const a1 = {
+  name: "Ali",
+  section: 1,
+  isPrincipal: false
+}
+
+a1['friend'] = "Ali"  
+a1['name'] = "Adeel"  
+console.log(a1)
+
+const dict = {
+  appreciate: "recognize the full worth of.",
+  ataraxia: "a state of freedom from emotional disturbance and anxiety",
+  yakka: "work, especially hard work."
+}
+
+console.log(dict['yakka'])
---
+++ var-let-const.js
@@ -0,0 +1,13 @@
+console.log("JavaScript tutorial 3: var, let and const")
+let b = "Harry";
+const author = "Harry"
+author = 5 
+b = 4
+const harry = 0;
+let c = null
+let d  = undefined  
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
