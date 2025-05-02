be74aa5871667be2afc19e4cbe6c41aa2cc7d68e
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-05-02 18:27:14 +0500

    solved first 2 quiz

---
+++ Operation.js
@@ -0,0 +1,49 @@
+//arthematic operators
+let a = 44
+let b = 66
+console.log("a + b =",a+b)
+console.log("a - b =",a-b)
+console.log("a / b =",a/b)
+console.log("a * b =",a*b)
+console.log("a ** b =",a**b)
+console.log("a % b =",a%b)
+console.log("a++ =",a++)
+console.log("++a =",++a)
+console.log("a-- =",a--)
+console.log("--a =",--a)
+//assignment operators
+let d= 1
+d += 5
+console.log("a is now", a)
+
+d -= 5
+console.log("a is now", a)
+
+d /= 5
+console.log("a is now", a)
+
+d *= 5
+console.log("a is now", a)
+
+d **= 5
+console.log("a is now", a)
+
+//comparision operators
+let c1 = 6
+let c2 = 7
+console.log("c1 == c2 is" ,c1 == c2)
+console.log("c1 != c2 is" ,c1 != c2)
+console.log("c1 === c2 is" ,c1 === c2)
+console.log("c1 !== c2 is" ,c1 !== c2)
+console.log("c1 > c2 is" ,c1 > c2)
+console.log("c1 < c2 is" ,c1 < c2)
+console.log("c1 > c2 is" ,c1 >= c2)
+console.log("c1 < c2 is" ,c1 <= c2)
+
+//logical opreators 
+let x = 33
+let y = 34
+console.log(x<y && x==33)
+console.log(x<y || x==33)
+console.log(!false)
+console.log(!true )
