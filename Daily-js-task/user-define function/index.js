455e54b2c2ddc4cfbb5d9c0df9d64a86ad85098b
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-05-14 14:01:07 +0500

    task completed

--- Daily-js-task/user-define function/index.js
+++ Daily-js-task/user-define function/index.js
@@ -1,41 +1,124 @@
-function checkMatricResult(matricMarks) {
+console.log("Student Info");
+
+
+function Result(matricMarks) {
     let percentage = (matricMarks / 1000) * 100;
     console.log(`Matric Percentage: ${percentage}%`);
-    console.log(percentage >= 33 ? "Pass" : "Fail");
+    if (percentage >= 33) {
+        console.log(`Pass`)
+    }
+    else {
+        console.log(`Fail`)
+    }
 }
 
+
 let firstname = prompt("Enter your firstname");
-let lastname = prompt("Enter your lastname");
-let phoneNumber = prompt("Enter your phonenumber");
-let class1 = prompt("Enter your class");
+
+
+if (/^[A-Za-z]+$/.test(firstname)  ) {
+
+    console.log(`First Name: ${firstname}`);
+} else if (firstname === "") {
+    console.log("Input can't be empty");
+}
+else{
+    console.log(`Please enter valid first name`)
+}
+
+
+let lastname = prompt(`Enter your lastname`);
+
+if (/^[A-Za-z]+$/.test(lastname)) {
+    console.log(`Last Name: ${lastname}`);
+} else if (lastname ==="") {
+    console.log("input can't be empty");
+}
+else{
+    console.log(`Please enter valid firstname`)
+}
+
+
 let city = prompt("Enter your city");
-let matricMarks = Number(prompt("Enter your matric marks (out of 1000):"));
 
-let marks1 = Number(prompt("Enter the marks of Chemistry"));
-let marks2 = Number(prompt("Enter the marks of Physics"));
-let marks3 = Number(prompt("Enter the marks of English"));
-let marks4 = Number(prompt("Enter the marks of Computer"));
-let marks5 = Number(prompt("Enter the marks of Urdu"));
-
-
-let totalMarks = marks1 + marks2 + marks3 + marks4 + marks5;
-let percentage = (totalMarks / 500) * 100;
+if (/^[A-Za-z]+$/.test(city)) {
+    console.log(`City: ${city}`);
+} else if (city=== "") {
+    console.log("Input can't be empty");
+}
+else{
+    console.log(`Please enter valid city name`)
+}
 
 
-console.log("Student Info");
-console.log(`First Name: ${firstname}`);
-console.log(`Last Name: ${lastname}`);
-console.log(`Phone Number: ${phoneNumber}`);
+let phoneNumber = Number(prompt("Enter your phone number"));
+
+if (phoneNumber === 0) {
+    console.log("Phone number should be equal to 11 digits");
+} else if (phoneNumber.toString().length > 11) {
+    console.log("Number should be equal to 11 digits");
+} else {
+    console.log(`Phone Number: ${phoneNumber}`);
+
+}
+
+let class1 = Number(prompt("Enter your class in digits"));
 console.log(`Class: ${class1}`);
-console.log(`City: ${city}`);
-console.log(`Chemistry: ${marks1}`);
-console.log(`Physics: ${marks2}`);
-console.log(`English: ${marks3}`);
-console.log(`Computer: ${marks4}`);
-console.log(`Urdu: ${marks5}`);
-console.log(`Total Marks: ${totalMarks}/500`);
-console.log(`Subject Percentage: ${percentage.toFixed(2)}%`);
-console.log(percentage >= 33 ? "Pass" : "Fail");
 
 
-checkMatricResult(matricMarks);
+
+let matricMarks = Number(prompt("Enter your matric marks (out of 1000):"));
+if (matricMarks <= 0 || matricMarks > 1000) {
+    console.log("Invalid numbers")
+}
+else{console.log(`Matric marks ${matricMarks}`)}
+
+let marks1 = Number(prompt("Enter the marks of Chemistry", 0, 100));
+if (marks1 <= 0 || marks1 > 100) {
+    console.log("Enter Vaild numbers of chemistry")
+} else {
+    console.log(`Chemistry: ${marks1}`);
+
+}
+
+
+let marks2 = Number(prompt("Enter the marks of Physics", 0, 100));
+if (marks2 <= 0 || marks2 > 100) {
+    console.log("Enter Vaild numbers of physics")
+} else {
+    console.log(`Physics: ${marks2}`);
+
+}
+
+let marks3 = Number(prompt("Enter the marks of English", 0, 100));
+if (marks3 <= 0 || marks3 > 100) {
+    console.log("Enter Vaild numbers of english")
+} else {
+    console.log(`English: ${marks3}`);
+
+}
+let marks4 = Number(prompt("Enter the marks of Computer", 0, 100));
+if (marks4 <= 0 || marks4 > 100) {
+    console.log("Enter Vaild numbers computer")
+} else {
+    console.log(`Computer: ${marks4}`);
+
+}
+let marks5 = Number(prompt("Enter the marks of Urdu", 0, 100));
+if (marks5 <= 0 || marks5 > 100) {
+    console.log("Enter Vaild numbers urdu")
+} else {
+    console.log(`Urdu: ${marks5}`);
+
+}
+let totalMarks = marks1 + marks2 + marks3 + marks4 + marks5;
+
+if (totalMarks > 500) {
+    console.log(`Enter valid marks in subjects`)
+}
+else{console.log(`Total Marks: ${totalMarks}/500`);}
+
+
+
+
+Result(matricMarks);
