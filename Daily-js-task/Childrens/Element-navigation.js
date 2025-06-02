21bd1b0f03f9586f175fb97693bf7c9da2f73491
Abdul Rehman Afzal <abdulrehmanafzal60@gmail.com>
2025-06-02 19:45:15 +0500

    task completed

---
+++ Daily-js-task/Childrens/Element-navigation.js
@@ -0,0 +1,3 @@
+ let b = document.body
+ console.log("First child of body:" , b.firstChild)
+ console.log("First element child of body:" ,b.firstElementChild)
---
+++ Daily-js-task/Childrens/acessing-childrens.js
@@ -0,0 +1,5 @@
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+console.log(document.body.childNodes[0])
+console.log(document.body.childNodes[document.body.childNodes.length- 1])
